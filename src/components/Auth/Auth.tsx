6fd80b7166d8a206fa5bb143298a21c9d0ea6b0f
mordobiv <mordobiv@gmail.com>
2023-05-25 19:03:10 +0200

    fix: remove unused functionality

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -6,28 +6,8 @@ import SignUp from "./SignUp";
 import {useLocation} from 'react-router-dom';
 
 export default function Auth() {
-  const [user, loading, error] = useAuthState(auth);
+  const [user] = useAuthState(auth);
   const location = useLocation().pathname;
-  // let isLogin, isRegister, isForgotPassword, isWelcome, isGraphiQl = false;
-  let isLogin = true;
-  
-  // switch (location) {
-  //   case '/login':
-  //     isLogin = true;
-  //     break;
-  //   case '/register':
-  //     isRegister = true;
-  //     break;
-  //   case '/reset':
-  //     isForgotPassword = true;
-  //     break;
-  //   case '/welcome':
-  //     isWelcome = true;
-  //     break;
-  //   case '/':
-  //     isGraphiQl = true;
-  //     break;
-  // }
 
   return (
     <>
