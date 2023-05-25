eabc05c2e74327892d018a6ea3f2393002304d86
mordobiv <mordobiv@gmail.com>
2023-05-25 20:56:39 +0200

    feat: welcome template

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -2,7 +2,35 @@ export default function Welcome() {
   return (
     <>
       <div>
-        Welcome
+        Welcome!
+      </div>
+      <div>
+        <div>RS School</div>
+        <div>React FE 2023 Q1</div>
+        <div>
+          <div>Start date: </div>
+          <div>13.03.2023</div>
+        </div>
+      </div>
+      <div>
+        <div>GraphiQL project</div>
+        <div>
+          GraphQL is an open-source data query and manipulation language for APIs and a query runtime engine. 
+          GraphQL enables declarative data fetching where a client can specify exactly what data it needs from an API. 
+        </div>
+      </div>
+      <div>
+        <div>
+          Team:
+        </div>
+        <div>
+          <div>Dzmitry Aniskovich</div>
+        </div>
+        <div>
+          <div>
+            Miras Suleymen
+          </div>
+        </div>
       </div>
     </>
   );
